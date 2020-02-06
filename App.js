30c1e11a1ead7a8177c3be27ac1b719e6896e5d3
vlad.kupnyy <vlad.kupnyy@gmail.com>
2020-02-06 21:18:45 +0200

    fix request

--- App.js
+++ App.js
@@ -3,31 +3,6 @@ import {View, Text} from 'react-native';
 import AppNavigator from './src/navigation/AppNavigator';
 
 const App = () => {
-  const [backdoor, changeBackdoor] = useState();
-  useEffect(() => {
-    const checkStatus = async () => {
-      try {
-        const result = await fetch(
-          'https://asist-17199.firebaseio.com/projects/Canaho.json',
-        );
-        // console.log(result)
-        // console.log(await result.json())
-        const status = await result.json();
-        changeBackdoor(status.status);
-      } catch (e) {
-        console.log(e);
-      }
-    };
-    checkStatus();
-  }, []);
-  if (backdoor === 'mudak') {
-    return (
-      <View
-        style={{flex: 1, additionalItems: 'center', justifyContent: 'center'}}>
-        <Text>Ooops, you forgot to pay for work </Text>
-      </View>
-    );
-  }
   return <AppNavigator />;
 };
 
